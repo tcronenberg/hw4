61b6b5ddf14e0435f00ce5a130ff4fb414940d7a
Tom Cronenberg <tom.cronenberg@hhu.de>
2015-11-20 13:53:08 +0100

    finished the homework

--- filter.cxx
+++ filter.cxx
@@ -6,28 +6,27 @@ using namespace std;
 
 
 void determine_size(const string, int&);
-
-void reading(const string, double*, int);
-
+double* reading(const string, int);
 void filter(double*, int);
-
-void savetofile(double*, int);
+void savetofile(double*, int, const string);
 
 int main(){
-    string filename = "noisy.txt";
+    const string filename_input = "noisy.txt";
+    const string filename_output = "filtered.txt";
+    const int Nfilter = 1;	//determines how often the filter is applied
     int Nlines = 0;
+    double* p;
+ 
   
-    determine_size(filename,Nlines);
-    double* p = new double[Nlines];
-    cout << Nlines << endl;
+    determine_size(filename_input,Nlines); 	//function which determines the number of lines of the file filename_input
+
+    p = reading(filename_input,Nlines);		//function which reads the data into a dynamic array and returns the pointer
     
-    reading(filename,p,Nlines);
-    
-    for(int i=1;i<=1;i++){
-    filter(p,Nlines);
+    for(int i=1;i<=Nfilter;i++){
+    filter(p,Nlines);	 //filter function
     }
     
-    savetofile(p,Nlines);
+    savetofile(p,Nlines,filename_output);	//save function
     
     delete[] p;
     return 0;
@@ -35,39 +34,42 @@ int main(){
 
 void determine_size(const string fname, int& Nlines){
     string templine;
+    
     ifstream in(fname.c_str());
     while (getline(in,templine))
 	Nlines++;
     in.close();
 }
 
-void reading(const string fname, double* p, int N){
+double* reading(const string fname, int N){
+    double* pn = new double[N];
     
     ifstream in(fname.c_str());
     for(int i=0; i<N; i++)
-	in >> p[i];
+	in >> pn[i];
     in.close();
+    
+    return pn;
 }
 
 void filter(double* p, int N){
     double temp1,temp2,tempN;
-    tempN = p[0];
     
-    	    temp1 = p[0];
-	    p[0] = (p[N-1]+p[0]+p[0+1])/3.0;
+    tempN = p[0];
+    temp1 = p[0];
+    p[0] = (p[N-1]+p[0]+p[0+1])/3.0;
 	    
     for(int i=1; i<N-1; i++){
 	  temp2 = p[i];
 	  p[i] = (temp1+p[i]+p[i+1])/3.0;
 	  temp1 = temp2;
-	  cout << i << endl;
     }
+    
     p[N-1] = (temp1+p[N-1]+tempN)/3.0;
-  
 }
 
-void savetofile(double* p, int N){
-    ofstream out("filtered.txt");
+void savetofile(double* p, int N, const string fname){
+    ofstream out(fname.c_str());
     for(int i=0; i<N; i++)
 	out << p[i] << endl;
     out.close();
